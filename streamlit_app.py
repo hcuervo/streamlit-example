8e98a2781ca6d6ef2418bf71565232864473cb4b
Horacio Cuervo <horaciocuervo@gmail.com>
2022-02-09 19:25:37 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,61 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
+    
+# Importar librerias
+import requests
+
+from datetime import datetime
+
+import pandas as pd
+import json as json
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from bs4 import BeautifulSoup # fundamental para scrapping
+
+# Scrapear de RAVA Online
+
+def historico(ticket=''):
+    # Link a la página que contiene la tabla
+    # Atención: dolar-plus.com entrega algo más de un año de historico
+    url  = 'https://www.rava.com/perfil/'+ticket
+
+    # Cargar la página
+    resp = requests.get(url)
+
+    # Interpretar la respuesta
+    soup = BeautifulSoup(resp.text, "html.parser")
+    
+    # Armar la respuesta según la página de RAVA Online
+    res = soup.find('perfil-p')[':res']
+    data = json.loads(res)
+    
+    
+    df=pd.DataFrame()
+    
+
+    df = pd.json_normalize(data['coti_hist'])
+    df.set_index('fecha',inplace=True)
+
+    X = []
+    Y = []
+    
+    if type(data['grafico_intradiario']) == list:
+        for each in data['grafico_intradiario']:
+            date = datetime.strptime(each['fecha']+' '+each['hora'], '%Y-%m-%d %H:%M:%S')
+            X.append(date)
+            Y.append(float(each['ultimo']))
+
+    intra = pd.DataFrame()
+    intra['time']=X
+    intra['price']=Y
+    intra.set_index('time',inplace=True)
+
+    
+    return(df,intra)
+
+h,i=historico('tx26')
+h.cierre.plot()
+i.plot()
