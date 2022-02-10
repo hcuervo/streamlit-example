f3001522338c5e4304e6ccf6f3236d2b4ce9399f
Horacio Cuervo <horaciocuervo@gmail.com>
2022-02-09 21:42:37 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,10 +68,6 @@ def historico(ticket=''):
     
 h,i=historico('tx26')
 
-print(h)
-h.cierre.plot()
-i.plot()
-
 total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
 num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
