1e40014ed4bbea37b0ce0679825ae2d0abb93ca1
Horacio Cuervo <horaciocuervo@gmail.com>
2022-02-09 21:48:47 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,9 +88,7 @@ st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
     .mark_circle(color='#0068c9', opacity=0.5)
     .encode(x='x:Q', y='y:Q'))
 
-st.altair_chart(alt.Chart(h)
-    .mark_circle(color='#0068c9', opacity=0.5)
-    .encode(x='x:Q', y='y:Q'))
+st.altair_chart(alt.Chart(h),mark='line',x=h.index, y='price')
 
 
 
