b99a56e661fe64ed881f51126a18ceb2270ff004
Horacio Cuervo <horaciocuervo@gmail.com>
2022-09-28 18:09:53 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,180 +1,123 @@
-stock_tickers = ['AAPL', 'GOOGL','PG','TCS.BO','MSFT','TWTR','NFLX','INFY.BO','WIPRO.BO','SBIN.NS',
-                'HDFC.NS','ICICIBANK.NS','RELIANCE.NS','TECHM.NS','HINDUNILVR.NS','BHARTIARTL.NS',
-                'HCLTECH.NS','ASIANPAINT.NS','LT.NS','AXISBANK.NS','MARUTI.NS','TATAMOTORS.NS','TATASTEEL.NS',
-                'NESTLEIND.NS','AMZN','ACN','ADBE','ORCL','FB','CIPLA6.BO','KPITTECH.NS','BSOFT.NS','LTI.NS','MINDTREE.BO','MPHASIS.NS','NAZARA.NS','NAUKRI.NS',
-                'TATAELXSI.NS','HAPPSTMNDS.NS','LUPIN.NS','GLAND.NS','ADANITRANS.NS','ADANIGREEN.NS']
+# @Email:  contact@pythonandvba.com
+# @Website:  https://pythonandvba.com
+# @YouTube:  https://youtube.com/c/CodingIsFun
+# @Project:  Sales Dashboard w/ Streamlit
 
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 12 17:57:42 2021
-@author: Teo Bee Guan
-"""
 
-import pandas as pd
+
+import pandas as pd  # pip install pandas openpyxl
+import numpy as np
+import statsmodels.api as sm
+import plotly.express as px  # pip install plotly-express
+import streamlit as st  # pip install streamlit
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 import yfinance as yf
-import streamlit as st
-import datetime as dt
-#import plotly.graph_objects as go
-#from plotly.subplots import make_subplots
-import numpy as np
 
-snp500 = pd.read_csv("Datasets/SP500.csv")
-symbols = snp500['Symbol'].sort_values().tolist()
+periods = [90,30,10,3]
+tickets = ['AAPL','YPF','GGAL.BA','SBUX']
 
-st.set_page_config(
-    page_title="Market Profile Chart (US S&P 500)",
-    layout="wide")
+# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
 
+# ---- GET DATA ----
+#@st.cache
+def get_data():
+    hist = yf.download(tickets,period='1y')
+    return(hist.Close)
 
-ticker = st.sidebar.selectbox(
-    'Choose a S&P 500 Stock',
-     symbols)
+def tendencia(df2,periods=periods):
+    df2 = df2.tail(max(periods)).fillna(method='bfill')
 
-i = st.sidebar.selectbox(
-        "Interval in minutes",
-        ("1m", "5m", "15m", "30m")
-    )
+    lista = []
+    for period in periods:
+        # Calcular tendencias
+        Y = df2.tail(period).iloc[:,0]
+        X = pd.to_numeric(df2.tail(period).index)
+        X = sm.add_constant(range(len(df2.tail(period).index)),prepend=True)
+        model = sm.OLS(Y,X)
+        results = model.fit()
+        trend = results.fittedvalues
+        #lista.append(trend)
+        df2 = pd.concat([df2,pd.DataFrame(trend,columns=[period])],axis=1)
 
-p = st.sidebar.number_input("How many days (1-30)", min_value=1, max_value=30, step=1)
-
-stock = yf.Ticker(ticker)
-history_data = stock.history(interval = i, period = str(p) + "d")
-
-prices = history_data['Close']
-volumes = history_data['Volume']
-
-lower = prices.min()
-upper = prices.max()
-
-prices_ax = np.linspace(lower,upper, num=20)
-
-vol_ax = np.zeros(20)
-
-for i in range(0, len(volumes)):
-    if(prices[i] >= prices_ax[0] and prices[i] < prices_ax[1]):
-        vol_ax[0] += volumes[i]   
-        
-    elif(prices[i] >= prices_ax[1] and prices[i] < prices_ax[2]):
-        vol_ax[1] += volumes[i]  
-        
-    elif(prices[i] >= prices_ax[2] and prices[i] < prices_ax[3]):
-        vol_ax[2] += volumes[i] 
-        
-    elif(prices[i] >= prices_ax[3] and prices[i] < prices_ax[4]):
-        vol_ax[3] += volumes[i]  
-        
-    elif(prices[i] >= prices_ax[4] and prices[i] < prices_ax[5]):
-        vol_ax[4] += volumes[i]  
-        
-    elif(prices[i] >= prices_ax[5] and prices[i] < prices_ax[6]):
-        vol_ax[5] += volumes[i] 
-        
-    elif(prices[i] >= prices_ax[6] and prices[i] < prices_ax[7]):
-        vol_ax[6] += volumes[i] 
-
-    elif(prices[i] >= prices_ax[7] and prices[i] < prices_ax[8]):
-        vol_ax[7] += volumes[i] 
-
-    elif(prices[i] >= prices_ax[8] and prices[i] < prices_ax[9]):
-        vol_ax[8] += volumes[i] 
-
-    elif(prices[i] >= prices_ax[9] and prices[i] < prices_ax[10]):
-        vol_ax[9] += volumes[i] 
-
-    elif(prices[i] >= prices_ax[10] and prices[i] < prices_ax[11]):
-        vol_ax[10] += volumes[i] 
-
-    elif(prices[i] >= prices_ax[11] and prices[i] < prices_ax[12]):
-        vol_ax[11] += volumes[i] 
-
-    elif(prices[i] >= prices_ax[12] and prices[i] < prices_ax[13]):
-        vol_ax[12] += volumes[i] 
-
-    elif(prices[i] >= prices_ax[13] and prices[i] < prices_ax[14]):
-        vol_ax[13] += volumes[i] 
-
-    elif(prices[i] >= prices_ax[14] and prices[i] < prices_ax[15]):
-        vol_ax[14] += volumes[i]   
-        
-    elif(prices[i] >= prices_ax[15] and prices[i] < prices_ax[16]):
-        vol_ax[15] += volumes[i] 
-        
-    elif(prices[i] >= prices_ax[16] and prices[i] < prices_ax[17]):
-        vol_ax[16] += volumes[i]         
-        
-    elif(prices[i] >= prices_ax[17] and prices[i] < prices_ax[18]):
-        vol_ax[17] += volumes[i]         
-        
-    elif(prices[i] >= prices_ax[18] and prices[i] < prices_ax[19]):
-        vol_ax[18] += volumes[i] 
-    
-    else:
-        vol_ax[19] += volumes[i]
-        
-fig = make_subplots(
-        rows=1, cols=2,
-        column_widths=[0.2, 0.8],
-        specs=[[{}, {}]],
-        horizontal_spacing = 0.01
-    
-    )
-
-fig.add_trace(
-        go.Bar(
-                x = vol_ax, 
-                y= prices_ax,
-                text = np.around(prices_ax,2),
-                textposition='auto',
-                orientation = 'h'
-            ),
-        
-        row = 1, col =1
-    )
+    return(df2)    
 
 
-dateStr = history_data.index.strftime("%d-%m-%Y %H:%M:%S")
+df = get_data()
 
-fig.add_trace(
-    go.Candlestick(x=dateStr,
-                open=history_data['Open'],
-                high=history_data['High'],
-                low=history_data['Low'],
-                close=history_data['Close'],
-                yaxis= "y2"
-                
-            ),
-    
-        row = 1, col=2
-    )
-        
 
-fig.update_layout(
-    title_text='Market Profile Chart (US S&P 500)', # title of plot
-    bargap=0.01, # gap between bars of adjacent location coordinates,
-    showlegend=False,
-    
-    xaxis = dict(
-            showticklabels = False
-        ),
-    yaxis = dict(
-            showticklabels = False
-        ),
-    
-    yaxis2 = dict(
-            title = "Price (USD)",
-            side="right"
-        
-        )
+# ---- MAINPAGE ----
+st.title(":bar_chart: Ticker Trends")
+st.markdown("##")
 
+# ---- SIDEBAR ----
+st.sidebar.header("Please Filter Here:")
+ticker = st.sidebar.multiselect(
+    "Select the Ticker:",
+    options=df.columns.unique(),
+    #default=df.columns.unique()
 )
 
-fig.update_yaxes(nticks=20)
-fig.update_yaxes(side="right")
-fig.update_layout(height=800)
 
-config={
-        'modeBarButtonsToAdd': ['drawline']
-    }
+st.title(ticker)
 
-st.plotly_chart(fig, use_container_width=True, config=config)
+fig = plt.figure(figsize=(4,3))
+for each in ticker:
+    lista = tendencia(pd.DataFrame(df[each])).reset_index()
+    sns.scatterplot(data=lista, x='Date', y=each,alpha=0.5)
+    sns.lineplot(data=lista, x='Date', y=90)
+    sns.lineplot(data=lista, x='Date', y=30)
+    sns.lineplot(data=lista, x='Date', y=10)
+    sns.lineplot(data=lista, x='Date', y=3)
+
+    st.pyplot(fig)
+
+    fig = plt.figure(figsize=(4,3))
+
+
+
+
+
+
+
+
+
+
+# # TOP KPI's
+# total_sales = 1000
+# average_rating = 2000
+# star_rating = ":star:" * 3
+# average_sale_by_transaction = 800
+
+# left_column, middle_column, right_column = st.columns(3)
+# with left_column:
+#     st.subheader("Total Sales:")
+#     st.subheader(f"US $ {total_sales:,}")
+# with middle_column:
+#     st.subheader("Average Rating:")
+#     st.subheader(f"{average_rating} {star_rating}")
+# with right_column:
+#     st.subheader("Average Sales Per Transaction:")
+#     st.subheader(f"US $ {average_sale_by_transaction}")
+
+st.markdown("""---""")
+
+
+
+left_column, right_column = st.columns(2)
+#left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
+
+
+
+# ---- HIDE STREAMLIT STYLE ----
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
